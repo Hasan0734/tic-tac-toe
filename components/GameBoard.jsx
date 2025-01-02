f7a60f2f4c2b3e7d56f7f2196b350ad481014a24
Jahid Hasan <jahidhasan2856@gmail.com>
2025-01-02 17:18:43 +0600

    adding design

--- components/GameBoard.jsx
+++ components/GameBoard.jsx
@@ -1,13 +1,119 @@
-import React from 'react'
-import BoardCard from './BoardCard'
+import { Circle, X } from "lucide-react";
+import React, { useState } from "react";
 
-const GameBoard = () => {
+const GameBoard = ({ handlePlayer, selectPlayer }) => {
+  const data = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+  ];
+
+  const [records, setRecords] = useState([]);
+
+  const handleClick = (i, e) => {
+
+    console.log(e.target)
+
+    setRecords((pre) => [...pre, { [i]: player }]);
+    
+
+    // if (records.length < 9) {
+    //   setRecords((pre) => [...pre, { [i]: player }]);
+    //   handlePlayer();
+    //   return;
+    // }
+  };
+
+  console.log(records);
+
   return (
-    <div className='grid grid-cols-3 gap-5'>
-        {[1,2,3,4,5,6,7,8,9].map(() => <BoardCard/>)}
-       
-    </div>
-  )
-}
+    <div className="relative w-[216px] h-[216px]">
+      <svg className="stroke-primary stroke-[6px] h-full w-full">
+        <path
+          d="M108,83L6,83"
+          style={{
+            strokeDasharray: 102,
+            strokeDashoffset: 0,
+          }}
+        ></path>
+        <path
+          d="M108,153L6,153"
+          style={{
+            strokeDasharray: 102,
+            strokeDashoffset: 0,
+          }}
+        ></path>
+        <path
+          d="M108,83L210,83"
+          style={{
+            strokeDasharray: 102,
+            strokeDashoffset: 0,
+          }}
+        ></path>
+        <path
+          d="M108,153L210,153"
+          style={{
+            strokeDasharray: 102,
+            strokeDashoffset: 0,
+          }}
+        ></path>
+        <path
+          d="M73,118L73,16"
+          style={{
+            strokeDasharray: 102,
+            strokeDashoffset: 0,
+          }}
+        ></path>
+        <path
+          d="M143,118L143,16"
+          style={{
+            strokeDasharray: 102,
+            strokeDashoffset: 0,
+          }}
+        ></path>
+        <path
+          d="M73,118L73,220"
+          style={{
+            strokeDasharray: 102,
+            strokeDashoffset: 0,
+          }}
+        ></path>
+        <path
+          d="M143,118L143,220"
+          style={{
+            strokeDasharray: 102,
+            strokeDashoffset: 0,
+          }}
+        ></path>
+      </svg>
 
-export default GameBoard
+      <table className="absolute w-full h-full inset-0 left-[3px] top-[10px]">
+        <tbody>
+          {data.map((dt, i) => (
+            <tr className="" key={i}>
+              {dt.map((item) => (
+                <td className="" key={item}>
+                  <span
+                    className="flex justify-center items-center relative  p-2 size-[64px] bg-orange-300"
+                    onClick={(e) => handleClick(item, e)}
+                  >
+                    <X label={"X"}className="size-12 invisible absolute inset-0 w-full h-full" />
+                    <Circle label={"O"}className="size-12 invisible absolute inset-0 w-full h-full" />
+                  </span>
+                </td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+
+    // <div className='grid grid-cols-3 gap-5'>
+
+    //     {[1,2,3,4,5,6,7,8,9].map(() => <BoardCard/>)}
+
+    // </div>
+  );
+};
+
+export default GameBoard;
--- components/HomeSection.jsx
+++ components/HomeSection.jsx
@@ -1,12 +1,16 @@
-"use client"
+"use client";
 import React, { useState } from "react";
 import GameBoard from "./GameBoard";
 import { Button } from "./ui/button";
 import { Card } from "./ui/card";
+import { Circle, X } from "lucide-react";
+import { cn } from "@/lib/utils";
 
 const HomeSection = () => {
   const [newGame, setNewGame] = useState(false);
   const [resetGame, setResetGame] = useState(false);
+  const [selectPlayer, setSelectPlayer] = useState("X");
+  const [tiles, setTiles] = useState(Array(9).fill(null));
 
   const handleNewGame = () => {
     setNewGame(true);
@@ -16,14 +20,63 @@ const HomeSection = () => {
     setResetGame(true);
   };
 
+  const handleSelectPlayer = () => {
+    if (selectPlayer === "X") {
+      setSelectPlayer("O");
+      return;
+    } else {
+      setSelectPlayer("X");
+    }
+  };
+
   return (
     <>
       <div className="flex flex-col items-center gap-6">
         <h1 className="text-4xl font-semibold ">Tic Tac Toe</h1>
-        
-        <Card className="border rounded-md p-10 h-[450px] w-[450px] flex items-center justify-center flex-col">
-        {newGame && <GameBoard />}
-        {!newGame && <Button onClick={setNewGame}>New Game</Button>}
+
+        <Card className="border rounded-md p-10 w-[450px] flex items-center justify-center flex-col space-y-6">
+          {newGame && (
+            <>
+              <div className="text-center w-full space-y-4 border-b pb-2">
+                <div className="flex items-center w-full gap-4">
+                  <div
+                    onClick={handleSelectPlayer}
+                    className={cn(
+                      "flex-grow border rounded-md px-4 py-2 flex items-center justify-between cursor-pointer",
+                      { "border-b-primary border-b-4 ": selectPlayer === "X" }
+                    )}
+                  >
+                    <X className="size-6" />
+                    <span className="text-xl text-gray-400 font-semibold">
+                      1
+                    </span>
+                  </div>
+                  <div
+                    onClick={handleSelectPlayer}
+                    className={cn(
+                      "flex-grow border rounded-md px-4 py-2 flex items-center justify-between cursor-pointer",
+                      { "border-b-primary  border-b-4 ": selectPlayer === "O" }
+                    )}
+                  >
+                    <Circle className="size-6" />
+                    <span className="text-xl text-gray-400 font-semibold">
+                      -
+                    </span>
+                  </div>
+                </div>
+
+                <p className="text-sm text-muted-foreground">
+                  Start game or select player
+                </p>
+              </div>
+              <GameBoard
+                tiles={tiles}
+                selectPlayer={selectPlayer}
+                handlePlayer={handleSelectPlayer}
+              />
+            </>
+          )}
+          {!newGame && <Button onClick={setNewGame}>New Game</Button>}
         </Card>
       </div>
     </>
